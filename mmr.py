6536c8127023ac6f71b17e09a6ff739d0dfacf0b
Spencer Sochin <sochin.s@husky.neu.edu>
2023-12-24 15:49:45 -0500

    making script more robust. added some error handling, a new link that will work regardless of current season, and better comments

--- mmr.py
+++ mmr.py
@@ -4,31 +4,56 @@ from selenium import webdriver
 import json
 import sys
 
+DOUBLES_PLAYLIST_ID = 11
+STANDARD_PLAYLIST_ID = 13
+RUMBLE_PLAYLST_ID = 27
+
 def main(id_dict):
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    options.add_argument("--headless") # runs without opening a window for the browser
     driver = uc.Chrome(options=options, executable_path = "./chromedriver.exe")
 
-    # TODO: automate getting the current season for the URL
-
     for name in id_dict.keys():
-        # TODO: don't close and re-open driver every time, use a new URL instead
         steamid = id_dict[name]
-        url = f'https://api.tracker.gg/api/v2/rocket-league/standard/profile/steam/{steamid}/segments/playlist?season=27'
-        driver.get(url)
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        ranks_json = json.loads(soup.pre.get_text())
+        url = f'https://api.tracker.gg/api/v2/rocket-league/standard/profile/steam/{steamid}'
 
-        for playlists in ranks_json['data']:
-            if playlists['attributes']['playlistId'] == 11:
-                doubles_mmr = playlists['stats']['rating']['value']
-            elif playlists['attributes']['playlistId'] == 13:
-                standard_mmr = playlists['stats']['rating']['value']
-            elif playlists['attributes']['playlistId'] == 28:
-                rumble_mmr = playlists['stats']['rating']['value']
+        try:
+            driver.get(url)
+        except:
+            print("HTML GET failed, check the API URL.")
+            driver.quit()
+            return
+
+        # get html source
+        html = driver.page_source
+
+        # use bs4 to parse html because i'm lazy
+        parsed_html = BeautifulSoup(html, 'html.parser')
+
+        # json-ify text from "pre" section of html (where all the json info is)
+        try:
+            profile_json = json.loads(parsed_html.pre.get_text())
+        except:
+            print("JSON parsing error, check the API URL.")
+            driver.quit()
+            return
+
+        # get each mmr value based on the playlist ID
+        for playlist in profile_json['data']['segments']:
+
+            # skip overview segment, parse playlists only
+            if (playlist['type'] == 'overview'):
+                continue
+
+            if playlist['attributes']['playlistId'] == DOUBLES_PLAYLIST_ID:
+                doubles_mmr = playlist['stats']['rating']['value']
+            elif playlist['attributes']['playlistId'] == STANDARD_PLAYLIST_ID:
+                standard_mmr = playlist['stats']['rating']['value']
+            elif playlist['attributes']['playlistId'] == RUMBLE_PLAYLST_ID:
+                rumble_mmr = playlist['stats']['rating']['value']
 
         print(f'\n{name} mmr list\n----------------\n{doubles_mmr = }\n{standard_mmr = }\n{rumble_mmr = }\n')
+
     driver.quit()
 
 if __name__ == '__main__':
